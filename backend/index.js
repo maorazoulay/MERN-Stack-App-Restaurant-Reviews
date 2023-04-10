07c541a8638c4011b8c447c7ec8054cc60ca6636
maorazoulay <maorazoulay@gmail.com>
2023-04-10 18:09:05 -0400

    Added reviews injectDB

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ import app from './server.js'
 import mongodb from 'mongodb'
 import dotenv from 'dotenv'
 import RestaurantsDAO from './dao/restaurantsDAO.js'
+import ReviewsDAO from './dao/reviewsDAO.js'
 
 dotenv.config()
 const MongoClient = mongodb.MongoClient
@@ -18,7 +19,8 @@ MongoClient.connect(process.env.RESTREVIEWS_DB_URI, {
     process.exit(1)
 })
 .then(async client => {
-    RestaurantsDAO.injectDB(client)
+    await RestaurantsDAO.injectDB(client)
+    await ReviewsDAO.injectDB(client)
     app.listen(port, () => {
     console.log(`listening on ${port}`)       
     })
